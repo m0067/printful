eca1001620e3b6fbaa6342be2e272085d0122c7b
maksimus <maksimtsoi.nsk@gmail.com>
2022-01-07 23:09:37 +0600

    init app 7

--- app/tests/functional/PrintfulShippingOptionsTest.php
+++ app/tests/functional/PrintfulShippingOptionsTest.php
@@ -17,32 +17,43 @@ class PrintfulShippingOptionsTest extends TestCase
 
         $this->assertFileExists(self::FILE_PATH);
         $data = \json_decode($this->getActualOutputForAssertion(), true);
-        $this->assertIsString($data[0]['id']);
-        $this->assertIsString($data[0]['name']);
-        $this->assertIsString($data[0]['rate']);
-        $this->assertIsString($data[0]['currency']);
-        $this->assertIsInt($data[0]['minDeliveryDays']);
-        $this->assertIsInt($data[0]['maxDeliveryDays']);
+        $this->assertDataTypeStructure($data[0]);
     }
 
     public function testApiWithExpiredCache(): void
     {
         (new AddPrintfulShippingOptionsCommand)->execute();
+        $expiratedAt = $this->getExpiresAt(self::FILE_PATH);
         \sleep(3);
         (new AddPrintfulShippingOptionsCommand)->execute();
+        $newExpiresAt = $this->getExpiresAt(self::FILE_PATH);
 
-        $this->assertFileExists(self::FILE_PATH);
+        $this->assertGreaterThan($expiratedAt, $newExpiresAt);
         $data = \json_decode(explode(PHP_EOL, $this->getActualOutputForAssertion())[1], true);
-        $this->assertIsString($data[0]['id']);
-        $this->assertIsString($data[0]['name']);
-        $this->assertIsString($data[0]['rate']);
-        $this->assertIsString($data[0]['currency']);
-        $this->assertIsInt($data[0]['minDeliveryDays']);
-        $this->assertIsInt($data[0]['maxDeliveryDays']);
+        $this->assertDataTypeStructure($data[0]);
     }
 
     protected function tearDown(): void
     {
         @\unlink(self::FILE_PATH);
+    }
+
+    private function assertDataTypeStructure(array $data): void
+    {
+        $this->assertIsString($data['id']);
+        $this->assertIsString($data['name']);
+        $this->assertIsString($data['rate']);
+        $this->assertIsString($data['currency']);
+        $this->assertIsInt($data['minDeliveryDays']);
+        $this->assertIsInt($data['maxDeliveryDays']);
+    }
+
+    private function getExpiresAt(string $filePath): int
+    {
+        $handle = @\fopen($filePath, 'r');
+        $expiresAt = (int)\fgets($handle);
+        \fclose($handle);
+
+        return $expiresAt;
     }
 }
